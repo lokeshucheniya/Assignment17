19cf89350403b471e37dced86ad3617f0c9d6d68
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-05 19:06:49 +0530

    Update CalculatorTest.java

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -2,6 +2,7 @@ package in.co.capgemini;
 
 public class CalculatorTest {
 
+	//Calcularor methods to perform different operations
 	public int add(int i, int j) {
 		// TODO Auto-generated method stub
 		return i+j;
