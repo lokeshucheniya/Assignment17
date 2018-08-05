2def41eda1daed746a877341694a1b3f2899b268
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-05 18:38:40 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -5,38 +5,43 @@ import org.junit.jupiter.api.Test;
 
 class Calculator {
 
+	//Test case for Addition
 	@Test
 	public void AdditionTest() {
 		CalculatorTest calculator = new CalculatorTest();
 		int ans = calculator.add(5,5);
-		Assert.assertEquals(10, ans);
+		Assert.assertEquals(10, ans);				//Checking whether value is equal to the calculated value
 	}
 	
+	//Test case for Subtraction
 	@Test
 	public void SubractionTest() {
 		CalculatorTest calculator = new CalculatorTest();
 		int ans = calculator.sub(5,5);
-		Assert.assertEquals(0, ans);
+		Assert.assertEquals(0, ans);				//Checking whether value is equal to the calculated value
 	}
 	
+	//Test case for Multiplying
 	@Test
 	public void MultiplyTest() {
 		CalculatorTest calculator = new CalculatorTest();
 		int ans = calculator.multiply(5,5);
-		Assert.assertEquals(25, ans);
+		Assert.assertEquals(25, ans);				//Checking whether value is equal to the calculated value
 	}
 	
+	//Test case for Division
 	@Test
 	public void DivisionTest() {
 		CalculatorTest calculator = new CalculatorTest();
 		int ans = calculator.divide(5,5);
-		Assert.assertEquals(1, ans);
+		Assert.assertEquals(1, ans);				//Checking whether value is equal to the calculated value
 	}
 	
+	//Test case for Modulus
 	@Test
 	public void ModulusTest() {
 		CalculatorTest calculator = new CalculatorTest();
 		int ans = calculator.mod(5,5);
-		Assert.assertEquals(0, ans);
+		Assert.assertEquals(0, ans);				//Checking whether value is equal to the calculated value
 	}
 }
